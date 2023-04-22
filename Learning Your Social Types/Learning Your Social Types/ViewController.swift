fe78b3c9ca9c7e19a676af3f00173fabeaf59ca2
MWebb77 <131550682+MWebb77@users.noreply.github.com>
2023-04-22 14:34:49 -0500

    Intial Xcode Project
    Blank Xcode document prepped for editing.

---
+++ Learning Your Social Types/Learning Your Social Types/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning Your Social Types
+//
+//  Created by Myah W on 4/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Learning Your Social Types/Learning Your Social TypesUITests/Learning_Your_Social_TypesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Learning_Your_Social_TypesUITestsLaunchTests.swift
+//  Learning Your Social TypesUITests
+//
+//  Created by Myah W on 4/22/23.
+//
+
+import XCTest
+
+final class Learning_Your_Social_TypesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
